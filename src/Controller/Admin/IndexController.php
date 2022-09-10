54a8c3f552ee2d9aec471a8b44e3c57fb8d57ce5
Jim Safley <jimsafley@gmail.com>
2022-09-10 12:29:31 -0400

    Continue development

--- src/Controller/Admin/IndexController.php
+++ src/Controller/Admin/IndexController.php
@@ -1,6 +1,7 @@
 <?php
 namespace InverseProperties\Controller\Admin;
 
+use Laminas\Form\Form;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
 
@@ -8,15 +9,24 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+        // Must use a generic form to get CSRF protection.
+        $form = $this->getForm(Form::class);
+
         if ($this->getRequest()->isPost()) {
-            $propertyPairs = $this->params()->fromPost('property_pairs', []);
-            $this->inverseProperties()->setPropertyPairs($propertyPairs);
-            $this->messenger()->addSuccess('Property pairs successfully updated'); // @translate
+            $form->setData($this->params()->fromPost());
+            if ($form->isValid()) {
+                $propertyPairs = $this->params()->fromPost('property_pairs', []);
+                $this->inverseProperties()->setPropertyPairs($propertyPairs);
+                $this->messenger()->addSuccess('Property pairs successfully updated'); // @translate
+                return $this->redirect()->toRoute();
+            } else {
+                $this->messenger()->addFormErrors($form);
+            }
         }
-        $propertyPairs = $this->inverseProperties()->getPropertyPairs();
 
         $view = new ViewModel;
-        $view->setVariable('propertyPairs', $propertyPairs);
+        $view->setVariable('form', $form);
+        $view->setVariable('propertyPairs', $this->inverseProperties()->getPropertyPairs());
         return $view;
     }
 }
--- src/ControllerPlugin/InverseProperties.php
+++ src/ControllerPlugin/InverseProperties.php
@@ -17,6 +17,9 @@ class InverseProperties extends AbstractPlugin
         $this->entityManager = $this->services->get('Omeka\EntityManager');
     }
 
+    /**
+     * Get property pairs.
+     */
     public function getPropertyPairs() : array
     {
         $dql = 'SELECT pp FROM InverseProperties\Entity\InversePropertiesPropertyPair pp';
@@ -31,20 +34,28 @@ class InverseProperties extends AbstractPlugin
         return $propertyPairs;
     }
 
+    /**
+     * Set property pairs.
+     */
     public function setPropertyPairs(array $propertyPairs) : void
     {
-        $dqlSelect = '
-        SELECT pp
+        // Note that owl:inverseOf is a symmetric property, meaning that the
+        // (p1,p2) pair is semantically equivalent to the (p2,p1) pair. We must
+        // account for this when selecting the pair from the database or we risk
+        // duplicating semantically equivalent pairs.
+        $dqlSelect = 'SELECT pp
         FROM InverseProperties\Entity\InversePropertiesPropertyPair pp
         WHERE (pp.p1 = :p1 AND pp.p2 = :p2)
         OR (pp.p1 = :p2 AND pp.p2 = :p1)';
-        $dqlDelete = '
-        DELETE FROM InverseProperties\Entity\InversePropertiesPropertyPair pp
-        WHERE pp.id NOT IN (:ids)';
-        // We must set a nonexistent ID or no persistent property pairs will be
-        // deleted if the user deletes all property pairs in the UI.
+        $selectQuery = $this->entityManager->createQuery($dqlSelect);
+        // We must set a nonexistent ID (0) or no existing property pairs will
+        // be deleted if the user deletes all property pairs in the UI.
         $retainIds = [0];
         foreach ($propertyPairs as $propertyPair) {
+            if (!(is_array($propertyPair) && isset($propertyPair['p1']) && isset($propertyPair['p2']))) {
+                // Invalid format.
+                continue;
+            }
             if ($propertyPair['p1'] == $propertyPair['p2']) {
                 // A property cannot be an inverse of itself.
                 continue;
@@ -52,25 +63,29 @@ class InverseProperties extends AbstractPlugin
             $p1 = $this->entityManager->find('Omeka\Entity\Property', $propertyPair['p1']);
             $p2 = $this->entityManager->find('Omeka\Entity\Property', $propertyPair['p2']);
             if (!($p1 && $p2)) {
-                // The properties must exist.
+                // Both properties must exist.
                 continue;
             }
-            $propertyPairEntities = $this->entityManager
-                ->createQuery($dqlSelect)
+            $propertyPairEntities = $selectQuery
                 ->setParameter('p1', $propertyPair['p1'])
                 ->setParameter('p2', $propertyPair['p2'])
                 ->getResult();
             if ($propertyPairEntities) {
+                // This pair already exists.
                 $retainIds[] = $propertyPairEntities[0]->getId();
             } else {
+                // This pair does not exist. Create it.
                 $propertyPairEntity = new InversePropertiesPropertyPair;
                 $propertyPairEntity->setP1($p1);
                 $propertyPairEntity->setP2($p2);
                 $this->entityManager->persist($propertyPairEntity);
+                // Must flush here so Doctrine generates the ID.
                 $this->entityManager->flush();
                 $retainIds[] = $propertyPairEntity->getId();
             }
         }
+        // Delete all pairs did not already exist and weren't newly created.
+        $dqlDelete = 'DELETE FROM InverseProperties\Entity\InversePropertiesPropertyPair pp WHERE pp.id NOT IN (:ids)';
         $this->entityManager
             ->createQuery($dqlDelete)
             ->setParameter('ids', $retainIds)
